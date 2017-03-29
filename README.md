57e747f6d12a99974b423c31e6193d86c2b53969
Procope <m.giulianelli.m@gmail.com>
2017-03-29 12:22:18 +0200

    The beginning

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # emo2vec
-Learning emotion-specific embeddings
+#####Learning emotion-specific embeddings.
+
+\ \ \ \ \ 
+
+####Code
+
+- `skipgram_ns.py` is a [Keras](https://keras.io/) implementation of the skip-gram model
+- `simple_classifier.py` is a trivial fuzzy classifier based on Emolex
+- `analysis.py` only computes, for now, agreement between gold standard and otherwise obtained labels
+
+\ \ 
+
+####Resources
+
+- **NRC Word-Emotion Association Lexicon** aka EmoLex: association of words with eight emotions (anger, fear, anticipation, trust, surprise, sadness, joy, and disgust) and two sentiments (negative and positive) manually annotated on Amazon's Mechanical Turk. The English version of this lexicon is used in emo2vec, it contains 14,182 unigrams.
+
+    - Crowdsourcing a Word-Emotion Association Lexicon, Saif Mohammad and Peter Turney, Computational Intelligence, 29 (3), 436-465, 2013.
+
+    - Emotions Evoked by Common Words and Phrases: Using Mechanical Turk to Create an Emotion Lexicon, Saif Mohammad and Peter Turney, In Proceedings of the NAACL-HLT 2010 Workshop on Computational Approaches to Analysis and Generation of Emotion in Text, June 2010, LA, California.
+
+- **Hashtag Emotion Corpus**: a corpus of 21051 Twitter posts labeled with eight emotions using emotion-word hashtags.
+
+    - Using Hashtags to Capture Fine Emotion Categories from Tweets. Saif M. Mohammad, Svetlana Kiritchenko, Computational Intelligence, in press.
+
+    - \#Emotional Tweets, Saif Mohammad, In Proceedings of the First Joint Conference on Lexical and Computational Semantics (*Sem), June 2012, Montreal, Canada.
+    
+Both available at http://saifmohammad.com/WebPages/lexicons.html.
---
+++ analysis.py
@@ -0,0 +1,70 @@
+import numpy as np
+from pickle import load
+
+RESOURCES_PATH = 'resources/'
+CORPUS_PATH = RESOURCES_PATH + 'twitter_corpus.txt'
+LEXICON_PATH = RESOURCES_PATH + 'emolex.txt'
+LABELS_PATH = RESOURCES_PATH + 'labels.p'
+NUM_SAMPLES = 10000
+
+label_to_index = {'anger': 0,
+                  'anticipation': 1,
+                  'disgust': 2,
+                  'fear': 3,
+                  'joy': 4,
+                  'sadness': 5,
+                  'surprise': 6,
+                  'trust': 7}
+
+
+def agreement(gold, other):
+    n = len(gold)
+    assert (n == len(other))
+    assert (n > 0)
+
+    count_agree = 0
+
+    for i in range(n):
+        if type(other[i]) is int:
+            count_agree += int(gold[i] == other[i])
+        elif type(other[i]) is list:
+            count_agree += int(gold[i] in other[i])  # allows fuzzy labels
+
+    return count_agree / n
+
+
+# Extract gold standard labels
+gold_labels = []
+with open(CORPUS_PATH, 'r') as f:
+    for line in f:
+        line_split = line[20:].split(sep='\t:: ')
+        gold_labels.append(label_to_index[line_split[1].strip()])
+N = len(gold_labels)
+
+# Load fuzzy labels obtained with trivial annotation (simple_classifier.py)
+fuzzy_labels = load(open(LABELS_PATH, 'rb'))
+trivial_labels = []
+
+# Assign labels with maximum probability
+for label_distribution in fuzzy_labels:
+    max_prob = np.amax(label_distribution)
+    if max_prob == 0:
+        trivial_labels.append(-1)  # no emotion detected
+    else:
+        # all emotions with max probability value
+        trivial_labels.append([j for j, k in enumerate(label_distribution) if k == max_prob])
+
+# Generate (pseudo-)random labels
+random_labels = []
+for x in range(N):
+    random_labels.append(np.random.randint(0, 8))
+
+# Compute agreement
+random_agreement = 0
+for i in range(NUM_SAMPLES):
+    random_agreement += agreement(gold_labels, random_labels)
+
+random_agreement /= NUM_SAMPLES
+
+print('Random labels agreement (mean of {0} samples): {1}'.format(NUM_SAMPLES, random_agreement))
+print('Trivial labels agreement: {}'.format(agreement(gold_labels, trivial_labels)))
